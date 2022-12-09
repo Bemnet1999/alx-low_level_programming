40d1880fc0bbc3a63cb111d7399d06267db74986
Bemnet1999 <beemnetzewdu09@gmail.com>
2022-12-09 14:39:33 -0800

    program that prints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	char d;
 
 	c = 'a';
-	d='A';
+	d= 'A';
 	while
 		(c <= 'z')
 		{
@@ -18,7 +18,7 @@ int main(void)
 			c++;
 		}
 	while
-		(d <= 'z')
+		(d <= 'Z')
 		{
 			putchar(d);
 			d++;
