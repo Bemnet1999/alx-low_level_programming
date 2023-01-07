a11431aafbae60186c5e014be09b4c96a2de326a
Bemnet1999 <beemnetzewdu09@gmail.com>
2023-01-07 14:49:53 -0800

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,6 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * prints - Kevin Mitnick quote
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-_puts("\"At the end of the day, my goal is best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day, my goal is to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
